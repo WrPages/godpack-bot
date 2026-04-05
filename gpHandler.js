7c9666c43fb1239717a2bdf5abdf51eb3b20c15a
WrPages <wenderramirez00@gmail.com>
2026-04-05 17:22:12 -0600

    Update gpHandler.js

--- gpHandler.js
+++ gpHandler.js
@@ -435,7 +435,7 @@ console.log("🔥 HANDLER READY");
 
   ].map(cmd => cmd.toJSON());
 
-  const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
+  const rest = new REST({ version: "10" }).setToken(client.token);
 
   try {
     console.log("🚀 Registrando comandos...");
