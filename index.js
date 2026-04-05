c0612edbc277e06c7d1618d2d99597e8d84b3be8
WrPages <wenderramirez00@gmail.com>
2026-04-05 06:27:20 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
-const { Client, GatewayIntentBits } = require("discord.js")
-
-const client = new Client({
-  intents: [GatewayIntentBits.Guilds]
-})
-
-client.on("ready", () => {
+client.on("ready", async () => {
   console.log("🔥 BOT FUNCIONA")
+
+  const { REST, Routes, SlashCommandBuilder } = require("discord.js")
+
+  const commands = [
+    new SlashCommandBuilder()
+      .setName("online")
+      .setDescription("test"),
+    new SlashCommandBuilder()
+      .setName("offline")
+      .setDescription("test")
+  ].map(c => c.toJSON())
+
+  const rest = new REST({ version: "10" }).setToken(process.env.TOKEN)
+
+  await rest.put(
+    Routes.applicationGuildCommands(
+      process.env.CLIENT_ID,
+      process.env.GUILD_ID
+    ),
+    { body: commands }
+  )
+
+  console.log("✅ COMANDOS SUBIDOS")
 })
-
-client.on("interactionCreate", async interaction => {
-  console.log("📩 INTERACTION DETECTADA")
-
-  if (!interaction.isChatInputCommand()) return
-
-  if (interaction.commandName === "online") {
-    return interaction.reply("FUNCIONA")
-  }
-})
-
-client.login(process.env.TOKEN)
