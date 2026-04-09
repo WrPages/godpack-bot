143b7be6f91ea98130eb58c981ebab8e513704cd
WrPages <wenderramirez00@gmail.com>
2026-04-08 21:00:13 -0600

    Update index.js

--- index.js
+++ index.js
@@ -350,13 +350,7 @@ client.once("ready", async () => {
 
   const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
 
- await rest.put(
-  Routes.applicationGuildCommands(
-    process.env.CLIENT_ID,
-    process.env.GUILD_ID
-  ),
-  { body: commands }
-);
+
 
  // 🗑️ BORRAR COMANDOS ANTIGUOS DEL SERVIDOR
  // try {
