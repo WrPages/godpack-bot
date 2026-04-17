70fb1f6858bf65c2ce7f678df473ac46689e0466
WrPages <wenderramirez00@gmail.com>
2026-04-16 23:36:35 -0600

    Update index.js

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const client = new Client({
 const TOKEN = process.env.TOKEN
 const GITHUB_TOKEN = process.env.GITHUB_TOKEN
 const API_URL = "https://add-ids.netlify.app/.netlify/functions/api"
-const PANEL_CHANNEL_ID = "ID_CANAL_PANEL"
+const PANEL_CHANNEL_ID = "1484015417411244082L"
 
 // ================= GROUP CONFIG =================
 
