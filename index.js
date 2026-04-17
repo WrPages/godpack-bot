c6f1acf35e996e718108cc2e3fd9384a4403bba2
WrPages <wenderramirez00@gmail.com>
2026-04-17 11:39:10 -0600

    Actualizar index.js

--- index.js
+++ index.js
@@ -63,6 +63,9 @@ const GROUP_CONFIG = {
 // ================= FILES =================
 
 const SCHEDULE_FILE = "./daily_schedules.json"
+const PANEL_DATA_FILE = "./panel_data.json"
+
+
 
 // ================= HELPERS =================
 
@@ -126,6 +129,16 @@ async function getUserGroup(interaction){
   )
   return role?.name || null
 }
+/// panel
+function loadPanelData(){
+  if(!fs.existsSync(PANEL_DATA_FILE)) return {}
+  return JSON.parse(fs.readFileSync(PANEL_DATA_FILE))
+}
+
+function savePanelData(data){
+  fs.writeFileSync(PANEL_DATA_FILE, JSON.stringify(data,null,2))
+}
+
 
 // ================= SCHEDULER =================
 
@@ -155,6 +168,8 @@ function startScheduler(){
 // ================= PANEL =================
 
 async function sendPanel(channel){
+
+  const panelData = loadPanelData()
 
   const embed = new EmbedBuilder()
     .setTitle("🎮 PANEL CONTROL")
@@ -187,7 +202,32 @@ async function sendPanel(channel){
     new ButtonBuilder().setCustomId("gp").setLabel("Add VIP").setStyle(ButtonStyle.Success)
   )
 
-  await channel.send({embeds:[embed],components:[row1,row2,row3,row4,row5]})
+  const panelPayload = {
+    embeds:[embed],
+    components:[row1,row2,row3,row4,row5]
+  }
+
+  try {
+
+    // 🔁 SI YA EXISTE → EDITAR
+    if(panelData.messageId){
+      const msg = await channel.messages.fetch(panelData.messageId)
+      await msg.edit(panelPayload)
+      console.log("♻️ Panel actualizado")
+      return
+    }
+
+  } catch(err){
+    console.log("Panel anterior no encontrado, creando nuevo...")
+  }
+
+  // 🆕 SI NO EXISTE → CREAR NUEVO
+  const newMsg = await channel.send(panelPayload)
+
+  panelData.messageId = newMsg.id
+  savePanelData(panelData)
+
+  console.log("✅ Panel creado y guardado")
 }
 
 // ================= READY =================
